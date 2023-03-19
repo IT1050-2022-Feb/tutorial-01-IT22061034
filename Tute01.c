a8f87f152d3ed5a0cd5706ab869387ed3cfc2699
IT22061034 <127839057+IT22061034@users.noreply.github.com>
2023-03-19 14:44:53 +0000

    Tute01.c

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,22 @@
 #include <stdio.h>
 
 int main() {
+
+  //declaring variables
+  int m1, m2;
+  float avg;
+
+  //Getting user inputs
+  printf("Enter the mark 1 : ");
+  scnaf_s("%d", &m1);
+
+  printf("Enter the mark 2 : ");
+  scanf_s("%d", &m2);
+
+  //calculation
+  avg = m1 + m2 / 2.0;
+
+  printf("The average is %.2f", avg);
   
   return 0;
 }
